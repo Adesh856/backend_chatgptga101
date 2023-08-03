108b5f8550c999e93173368368a6909ac1fb61b2
Adesh856 <adeshtayde6@gmail.com>
2023-08-03 20:39:26 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require("cors")
 require("dotenv").config()
 const app = express();
+app.use(express.json())
+app.use(cors())
 const port = 3000;
 const {createCompletionChatGTP} = require("./chatgptapi")
 
